17d01c4490319722e7de6f853c449076c2cde91d
flashmercurymcfly <ryan.tsao42@gmail.com>
2021-12-20 22:44:02 -0500

    Updated enemy sprites

--- src/sd/js/enemyData.js
+++ src/sd/js/enemyData.js
@@ -219,6 +219,10 @@ var enemyData = {
 		"name" : "mob_b",
 		"skin" : ["enemy_1027_mob_b"]
 	},
+	"mob_2" : {
+		"name" : "mob_2",
+		"skin" : ["enemy_1027_mob_2"]
+	},
 	"mocock" : {
 		"name" : "mocock",
 		"skin" : ["enemy_1028_mocock"]
@@ -290,6 +294,10 @@ var enemyData = {
 	"amraoe_b" : {
 		"name" : "amraoe_b",
 		"skin" : ["enemy_1036_amraoe_b"]
+	},
+	"amraoe_2" : {
+		"name" : "amraoe_2",
+		"skin" : ["enemy_1036_amraoe_2"]
 	},
 	"lunsabr" : {
 		"name" : "lunsabr",
@@ -363,6 +371,10 @@ var enemyData = {
 		"name" : "agent_b",
 		"skin" : ["enemy_1046_agent_b"]
 	},
+	"agent_2" : {
+		"name" : "agent_2",
+		"skin" : ["enemy_1046_agent_2"]
+	},
 	"sagent" : {
 		"name" : "sagent",
 		"skin" : ["enemy_1047_sagent"]
@@ -370,6 +382,10 @@ var enemyData = {
 	"sagent_b" : {
 		"name" : "sagent_b",
 		"skin" : ["enemy_1047_sagent_b"]
+	},
+	"sagent_2" : {
+		"name" : "sagent_2",
+		"skin" : ["enemy_1047_sagent_2"]
 	},
 	"hirman" : {
 		"name" : "hirman",
@@ -386,6 +402,10 @@ var enemyData = {
 	"eagent_b" : {
 		"name" : "eagent_b",
 		"skin" : ["enemy_1049_eagent_b"]
+	},
+	"eagent_2" : {
+		"name" : "eagent_2",
+		"skin" : ["enemy_1049_eagent_2"]
 	},
 	"lslime" : {
 		"name" : "lslime",
@@ -727,6 +747,738 @@ var enemyData = {
 		"name" : "mdgint_2",
 		"skin" : ["enemy_1092_mdgint_2"]
 	},
+	"ccsbr" : {
+		"name" : "ccsbr",
+		"skin" : ["enemy_1093_ccsbr"]
+	},
+	"ccspm" : {
+		"name" : "ccspm",
+		"skin" : ["enemy_1094_ccspm"]
+	},
+	"ccspm_2" : {
+		"name" : "ccspm_2",
+		"skin" : ["enemy_1094_ccspm_2"]
+	},
+	"ccripr" : {
+		"name" : "ccripr",
+		"skin" : ["enemy_1095_ccripr"]
+	},
+	"ccripr_2" : {
+		"name" : "ccripr_2",
+		"skin" : ["enemy_1095_ccripr_2"]
+	},
+	"ccwitch" : {
+		"name" : "ccwitch",
+		"skin" : ["enemy_1096_ccwitch"]
+	},
+	"ccwitch_2" : {
+		"name" : "ccwitch_2",
+		"skin" : ["enemy_1096_ccwitch_2"]
+	},
+	"cclmbjk" : {
+		"name" : "cclmbjk",
+		"skin" : ["enemy_1097_cclmbjk"]
+	},
+	"cclmbjk_2" : {
+		"name" : "cclmbjk_2",
+		"skin" : ["enemy_1097_cclmbjk_2"]
+	},
+	"cchmpn" : {
+		"name" : "cchmpn",
+		"skin" : ["enemy_1098_cchmpn"]
+	},
+	"cchmpn_2" : {
+		"name" : "cchmpn_2",
+		"skin" : ["enemy_1098_cchmpn_2"]
+	},
+	"nbkght" : {
+		"name" : "nbkght",
+		"skin" : ["enemy_1099_nbkght"]
+	},
+	"nbkght_2" : {
+		"name" : "nbkght_2",
+		"skin" : ["enemy_1099_nbkght_2"]
+	},
+	"scorpn" : {
+		"name" : "scorpn",
+		"skin" : ["enemy_1100_scorpn"]
+	},
+	"scorpn_2" : {
+		"name" : "scorpn_2",
+		"skin" : ["enemy_1100_scorpn_2"]
+	},
+	"plkght" : {
+		"name" : "plkght",
+		"skin" : ["enemy_1101_plkght"]
+	},
+	"plkght_2" : {
+		"name" : "plkght_2",
+		"skin" : ["enemy_1101_plkght_2"]
+	},
+	"plkght_3" : {
+		"name" : "plkght_3",
+		"skin" : ["enemy_1101_plkght_3"]
+	},
+	"sdkght" : {
+		"name" : "sdkght",
+		"skin" : ["enemy_1102_sdkght"]
+	},
+	"sdkght_2" : {
+		"name" : "sdkght_2",
+		"skin" : ["enemy_1102_sdkght_2"]
+	},
+	"wdkght" : {
+		"name" : "wdkght",
+		"skin" : ["enemy_1103_wdkght"]
+	},
+	"wdkght_2" : {
+		"name" : "wdkght_2",
+		"skin" : ["enemy_1103_wdkght_2"]
+	},
+	"wdkght_3" : {
+		"name" : "wdkght_3",
+		"skin" : ["enemy_1103_wdkght_3"]
+	},
+	"lfkght" : {
+		"name" : "lfkght",
+		"skin" : ["enemy_1104_lfkght"]
+	},
+	"lfkght_2" : {
+		"name" : "lfkght_2",
+		"skin" : ["enemy_1104_lfkght_2"]
+	},
+	"lfkght_3" : {
+		"name" : "lfkght_3",
+		"skin" : ["enemy_1104_lfkght_3"]
+	},
+	"tyokai" : {
+		"name" : "tyokai",
+		"skin" : ["enemy_1105_tyokai"]
+	},
+	"tyokai_2" : {
+		"name" : "tyokai_2",
+		"skin" : ["enemy_1105_tyokai_2"]
+	},
+	"byokai" : {
+		"name" : "byokai",
+		"skin" : ["enemy_1106_byokai"]
+	},
+	"uoffcr" : {
+		"name" : "uoffcr",
+		"skin" : ["enemy_1107_uoffcr"]
+	},
+	"uoffcr_2" : {
+		"name" : "uoffcr_2",
+		"skin" : ["enemy_1107_uoffcr_2"]
+	},
+	"uterer" : {
+		"name" : "uterer",
+		"skin" : ["enemy_1108_uterer"]
+	},
+	"uterer_2" : {
+		"name" : "uterer_2",
+		"skin" : ["enemy_1108_uterer_2"]
+	},
+	"uabone" : {
+		"name" : "uabone",
+		"skin" : ["enemy_1109_uabone"]
+	},
+	"uabone_2" : {
+		"name" : "uabone_2",
+		"skin" : ["enemy_1109_uabone_2"]
+	},
+	"uamord" : {
+		"name" : "uamord",
+		"skin" : ["enemy_1110_uamord"]
+	},
+	"uamord_2" : {
+		"name" : "uamord_2",
+		"skin" : ["enemy_1110_uamord_2"]
+	},
+	"ucommd" : {
+		"name" : "ucommd",
+		"skin" : ["enemy_1111_ucommd"]
+	},
+	"uamord_2" : {
+		"name" : "uamord_2",
+		"skin" : ["enemy_1111_ucommd_2"]
+	},
+	"emppnt" : {
+		"name" : "emppnt",
+		"skin" : ["enemy_1112_emppnt"]
+	},
+	"emppnt_2" : {
+		"name" : "emppnt_2",
+		"skin" : ["enemy_1112_emppnt_2"]
+	},
+	"empace" : {
+		"name" : "empace",
+		"skin" : ["enemy_1113_empace"]
+	},
+	"empace_2" : {
+		"name" : "empace_2",
+		"skin" : ["enemy_1113_empace_2"]
+	},
+	"rgrdmn" : {
+		"name" : "rgrdmn",
+		"skin" : ["enemy_1114_rgrdmn"]
+	},
+	"rgrdmn_2" : {
+		"name" : "rgrdmn_2",
+		"skin" : ["enemy_1114_rgrdmn_2"]
+	},
+	"embald" : {
+		"name" : "embald",
+		"skin" : ["enemy_1115_embald"]
+	},
+	"liprr" : {
+		"name" : "liprr",
+		"skin" : ["enemy_1116_liprr"]
+	},
+	"liprr_2" : {
+		"name" : "liprr_2",
+		"skin" : ["enemy_1116_liprr_2"]
+	},
+	"liiprr" : {
+		"name" : "liiprr",
+		"skin" : ["enemy_1117_liiprr"]
+	},
+	"liiprr_2" : {
+		"name" : "liiprr_2",
+		"skin" : ["enemy_1117_liiprr_2"]
+	},
+	"lidbox" : {
+		"name" : "lidbox",
+		"skin" : ["enemy_1118_lidbox"]
+	},
+	"lidbox_2" : {
+		"name" : "lidbox_2",
+		"skin" : ["enemy_1118_lidbox_2"]
+	},
+	"vofsd" : {
+		"name" : "vofsd",
+		"skin" : ["enemy_1119_vofsd"]
+	},
+	"vofsd_2" : {
+		"name" : "vofsd_2",
+		"skin" : ["enemy_1119_vofsd_2"]
+	},
+	"vofwiz" : {
+		"name" : "vofwiz",
+		"skin" : ["enemy_1120_vofwiz"]
+	},
+	"vofwiz_2" : {
+		"name" : "vofwiz_2",
+		"skin" : ["enemy_1120_vofwiz_2"]
+	},
+	"lifbos" : {
+		"name" : "lifbos",
+		"skin" : ["enemy_1121_lifbos"]
+	},
+	"lifbos_2" : {
+		"name" : "lifbos_2",
+		"skin" : ["enemy_1121_lifbos_2"]
+	},
+	"sphond" : {
+		"name" : "sphond",
+		"skin" : ["enemy_1122_sphond"]
+	},
+	"sphond_2" : {
+		"name" : "sphond_2",
+		"skin" : ["enemy_1122_sphond_2"]
+	},
+	"spsbr" : {
+		"name" : "spsbr",
+		"skin" : ["enemy_1123_spsbr"]
+	},
+	"spsbr_2" : {
+		"name" : "spsbr_2",
+		"skin" : ["enemy_1123_spsbr_2"]
+	},
+	"spbow" : {
+		"name" : "spbow",
+		"skin" : ["enemy_1124_spbow"]
+	},
+	"spbow_2" : {
+		"name" : "spbow_2",
+		"skin" : ["enemy_1124_spbow_2"]
+	},
+	"spshld" : {
+		"name" : "spshld",
+		"skin" : ["enemy_1125_spshld"]
+	},
+	"spshld_2" : {
+		"name" : "spshld_2",
+		"skin" : ["enemy_1125_spshld_2"]
+	},
+	"spslme" : {
+		"name" : "spslme",
+		"skin" : ["enemy_1126_spslme"]
+	},
+	"spslme_2" : {
+		"name" : "spslme_2",
+		"skin" : ["enemy_1126_spslme_2"]
+	},
+	"sptrkg" : {
+		"name" : "sptrkg",
+		"skin" : ["enemy_1127_sptrkg"]
+	},
+	"sptrkg_2" : {
+		"name" : "sptrkg_2",
+		"skin" : ["enemy_1127_sptrkg_2"]
+	},
+	"spmage" : {
+		"name" : "spmage",
+		"skin" : ["enemy_1128_spmage"]
+	},
+	"spmage_2" : {
+		"name" : "spmage_2",
+		"skin" : ["enemy_1128_spmage_2"]
+	},
+	"spklr" : {
+		"name" : "spklr",
+		"skin" : ["enemy_1129_spklr"]
+	},
+	"spklr_2" : {
+		"name" : "spklr_2",
+		"skin" : ["enemy_1129_spklr_2"]
+	},
+	"infstr" : {
+		"name" : "infstr",
+		"skin" : ["enemy_1130_infstr"]
+	},
+	"sbeast" : {
+		"name" : "sbeast",
+		"skin" : ["enemy_1131_sbeast"]
+	},
+	"sbeast_2" : {
+		"name" : "sbeast_2",
+		"skin" : ["enemy_1131_sbeast_2"]
+	},
+	"sarchn" : {
+		"name" : "sarchn",
+		"skin" : ["enemy_1132_sarchn"]
+	},
+	"sarchn_2" : {
+		"name" : "sarchn_2",
+		"skin" : ["enemy_1132_sarchn_2"]
+	},
+	"harchn" : {
+		"name" : "harchn",
+		"skin" : ["enemy_1133_harchn"]
+	},
+	"harchn_2" : {
+		"name" : "harchn_2",
+		"skin" : ["enemy_1133_harchn_2"]
+	},
+	"diamd" : {
+		"name" : "diamd",
+		"skin" : ["enemy_1134_diamd"]
+	},
+	"diamd_2" : {
+		"name" : "diamd_2",
+		"skin" : ["enemy_1134_diamd_2"]
+	},
+	"redman" : {
+		"name" : "redman",
+		"skin" : ["enemy_1135_redman"]
+	},
+	"redman_2" : {
+		"name" : "redman_2",
+		"skin" : ["enemy_1135_redman_2"]
+	},
+	"redace" : {
+		"name" : "redace",
+		"skin" : ["enemy_1136_redace"]
+	},
+	"redace_2" : {
+		"name" : "redace_2",
+		"skin" : ["enemy_1136_redace_2"]
+	},
+	"plasm" : {
+		"name" : "plasm",
+		"skin" : ["enemy_1137_plasm"]
+	},
+	"tumor" : {
+		"name" : "tumor",
+		"skin" : ["enemy_1138_tumor"]
+	},
+	"hologc" : {
+		"name" : "hologc",
+		"skin" : ["enemy_1139_hologc"]
+	},
+	"hologc_2" : {
+		"name" : "hologc_2",
+		"skin" : ["enemy_1139_hologc_2"]
+	},
+	"merhd" : {
+		"name" : "merhd",
+		"skin" : ["enemy_1140_merhd"]
+	},
+	"merhd_2" : {
+		"name" : "merhd_2",
+		"skin" : ["enemy_1140_merhd_2"]
+	},
+	"merbow" : {
+		"name" : "merbow",
+		"skin" : ["enemy_1141_merbow"]
+	},
+	"merbow_2" : {
+		"name" : "merbow_2",
+		"skin" : ["enemy_1141_merbow_2"]
+	},
+	"mershd" : {
+		"name" : "mershd",
+		"skin" : ["enemy_1142_mershd"]
+	},
+	"mershd_2" : {
+		"name" : "mershd_2",
+		"skin" : ["enemy_1142_mershd_2"]
+	},
+	"merrpg" : {
+		"name" : "merrpg",
+		"skin" : ["enemy_1143_merrpg"]
+	},
+	"merrpg_2" : {
+		"name" : "merrpg_2",
+		"skin" : ["enemy_1143_merrpg_2"]
+	},
+	"merlgd" : {
+		"name" : "merlgd",
+		"skin" : ["enemy_1144_merlgd"]
+	},
+	"merlgd_2" : {
+		"name" : "merlgd_2",
+		"skin" : ["enemy_1144_merlgd_2"]
+	},
+	"atkspd" : {
+		"name" : "atkspd",
+		"skin" : ["enemy_1145_atkspd"]
+	},
+	"atkspd_2" : {
+		"name" : "atkspd_2",
+		"skin" : ["enemy_1145_atkspd_2"]
+	},
+	"defspd" : {
+		"name" : "defspd",
+		"skin" : ["enemy_1146_defspd"]
+	},
+	"defspd_2" : {
+		"name" : "defspd_2",
+		"skin" : ["enemy_1146_defspd_2"]
+	},
+	"dshond" : {
+		"name" : "dshond",
+		"skin" : ["enemy_1147_dshond"]
+	},
+	"dshond_2" : {
+		"name" : "dshond_2",
+		"skin" : ["enemy_1147_dshond_2"]
+	},
+	"dssbr" : {
+		"name" : "dssbr",
+		"skin" : ["enemy_1148_dssbr"]
+	},
+	"dssbr_2" : {
+		"name" : "dssbr_2",
+		"skin" : ["enemy_1148_dssbr_2"]
+	},
+	"dsbow" : {
+		"name" : "dsbow",
+		"skin" : ["enemy_1149_dsbow"]
+	},
+	"dsbow_2" : {
+		"name" : "dsbow_2",
+		"skin" : ["enemy_1149_dsbow_2"]
+	},
+	"dsjely" : {
+		"name" : "dsjely",
+		"skin" : ["enemy_1150_dsjely"]
+	},
+	"dsjely_2" : {
+		"name" : "dsjely_2",
+		"skin" : ["enemy_1150_dsjely_2"]
+	},
+	"dslntc" : {
+		"name" : "dslntc",
+		"skin" : ["enemy_1151_dslntc"]
+	},
+	"dslntc_2" : {
+		"name" : "dslntc_2",
+		"skin" : ["enemy_1151_dslntc_2"]
+	},
+	"dsurch" : {
+		"name" : "dsurch",
+		"skin" : ["enemy_1152_dsurch"]
+	},
+	"dslntc_2" : {
+		"name" : "dslntc_2",
+		"skin" : ["enemy_1152_dsurch_2"]
+	},
+	"dsexcu" : {
+		"name" : "dsexcu",
+		"skin" : ["enemy_1153_dsexcu"]
+	},
+	"dsexcu_2" : {
+		"name" : "dsexcu_2",
+		"skin" : ["enemy_1153_dsexcu_2"]
+	},
+	"dsmant" : {
+		"name" : "dsmant",
+		"skin" : ["enemy_1154_dsmant"]
+	},
+	"dsrobt" : {
+		"name" : "dsrobt",
+		"skin" : ["enemy_1155_dsrobt"]
+	},
+	"hymhr" : {
+		"name" : "hymhr",
+		"skin" : ["enemy_1156_hymhr"]
+	},
+	"hymwr_2" : {
+		"name" : "hymwr_2",
+		"skin" : ["enemy_1157_hymwr"]
+	},
+	"divman" : {
+		"name" : "divman",
+		"skin" : ["enemy_1158_divman"]
+	},
+	"divman_2" : {
+		"name" : "divman_2",
+		"skin" : ["enemy_1158_divman_2"]
+	},
+	"swfmob" : {
+		"name" : "swfmob",
+		"skin" : ["enemy_1159_swfmob"]
+	},
+	"swfmob_2" : {
+		"name" : "swfmob_2",
+		"skin" : ["enemy_1159_swfmob_2"]
+	},
+	"hvyslr" : {
+		"name" : "hvyslr",
+		"skin" : ["enemy_1160_hvyslr"]
+	},
+	"hvyslr_2" : {
+		"name" : "hvyslr_2",
+		"skin" : ["enemy_1160_hvyslr_2"]
+	},
+	"tidmag" : {
+		"name" : "tidmag",
+		"skin" : ["enemy_1161_tidmag"]
+	},
+	"tidmag_2" : {
+		"name" : "tidmag_2",
+		"skin" : ["enemy_1161_tidmag_2"]
+	},
+	"magmot" : {
+		"name" : "magmot",
+		"skin" : ["enemy_1162_magmot"]
+	},
+	"magmot_2" : {
+		"name" : "magmot_2",
+		"skin" : ["enemy_1162_magmot_2"]
+	},
+	"hvymot" : {
+		"name" : "hvymot",
+		"skin" : ["enemy_1163_hvymot"]
+	},
+	"hvymot_2" : {
+		"name" : "hvymot_2",
+		"skin" : ["enemy_1163_hvymot_2"]
+	},
+	"bmbmot" : {
+		"name" : "bmbmot",
+		"skin" : ["enemy_1164_bmbmot"]
+	},
+	"bmbmot_2" : {
+		"name" : "bmbmot_2",
+		"skin" : ["enemy_1164_bmbmot_2"]
+	},
+	"bmbmot_3" : {
+		"name" : "bmbmot_3",
+		"skin" : ["enemy_1164_bmbmot_3"]
+	},
+	"duhond" : {
+		"name" : "duhond",
+		"skin" : ["enemy_1165_duhond"]
+	},
+	"duhond_2" : {
+		"name" : "duhond_2",
+		"skin" : ["enemy_1165_duhond_2"]
+	},
+	"dusbr" : {
+		"name" : "dusbr",
+		"skin" : ["enemy_1166_dusbr"]
+	},
+	"dusbr_2" : {
+		"name" : "dusbr_2",
+		"skin" : ["enemy_1166_dusbr_2"]
+	},
+	"dubow" : {
+		"name" : "dubow",
+		"skin" : ["enemy_1167_dubow"]
+	},
+	"dubow_2" : {
+		"name" : "dubow_2",
+		"skin" : ["enemy_1167_dubow_2"]
+	},
+	"dumage" : {
+		"name" : "dumage",
+		"skin" : ["enemy_1168_dumage"]
+	},
+	"dumage_2" : {
+		"name" : "dumage_2",
+		"skin" : ["enemy_1168_dumage_2"]
+	},
+	"duphx" : {
+		"name" : "duphx",
+		"skin" : ["enemy_1169_duphx"]
+	},
+	"duphx_2" : {
+		"name" : "duphx_2",
+		"skin" : ["enemy_1169_duphx_2"]
+	},
+	"dushld" : {
+		"name" : "dushld",
+		"skin" : ["enemy_1170_dushld"]
+	},
+	"dushld_2" : {
+		"name" : "dushld_2",
+		"skin" : ["enemy_1170_dushld_2"]
+	},
+	"durokt" : {
+		"name" : "durokt",
+		"skin" : ["enemy_1171_durokt"]
+	},
+	"durokt_2" : {
+		"name" : "durokt_2",
+		"skin" : ["enemy_1171_durokt_2"]
+	},
+	"dugago" : {
+		"name" : "dugago",
+		"skin" : ["enemy_1172_dugago"]
+	},
+	"dugago_2" : {
+		"name" : "dugago_2",
+		"skin" : ["enemy_1172_dugago_2"]
+	},
+	"duspfr" : {
+		"name" : "duspfr",
+		"skin" : ["enemy_1173_duspfr"]
+	},
+	"duspfr_2" : {
+		"name" : "duspfr_2",
+		"skin" : ["enemy_1173_duspfr_2"]
+	},
+	"duholy" : {
+		"name" : "duholy",
+		"skin" : ["enemy_1174_duholy"]
+	},
+	"duholy_2" : {
+		"name" : "duholy_2",
+		"skin" : ["enemy_1174_duholy_2"]
+	},
+	"dushdo" : {
+		"name" : "dushdo",
+		"skin" : ["enemy_1175_dushdo"]
+	},
+	"dushdo_2" : {
+		"name" : "dushdo_2",
+		"skin" : ["enemy_1175_dushdo_2"]
+	},
+	"dusocr" : {
+		"name" : "dusocr",
+		"skin" : ["enemy_1176_dusocr"]
+	},
+	"dusocr_2" : {
+		"name" : "dusocr_2",
+		"skin" : ["enemy_1176_dusocr_2"]
+	},
+	"dscorp" : {
+		"name" : "dscorp",
+		"skin" : ["enemy_1178_dscorp"]
+	},
+	"scorp_2" : {
+		"name" : "scorp_2",
+		"skin" : ["enemy_1178_dscorp_2"]
+	},
+	"aruarw" : {
+		"name" : "aruarw",
+		"skin" : ["enemy_1179_aruarw"]
+	},
+	"aruarw_2" : {
+		"name" : "aruarw_2",
+		"skin" : ["enemy_1179_aruarw_2"]
+	},
+	"aruass" : {
+		"name" : "aruass",
+		"skin" : ["enemy_1180_aruass"]
+	},
+	"aruass_2" : {
+		"name" : "aruass_2",
+		"skin" : ["enemy_1180_aruass_2"]
+	},
+	"napkgt" : {
+		"name" : "napkgt",
+		"skin" : ["enemy_1181_napkgt"]
+	},
+	"napkgt_2" : {
+		"name" : "napkgt_2",
+		"skin" : ["enemy_1181_napkgt_2"]
+	},
+	"flasrt" : {
+		"name" : "flasrt",
+		"skin" : ["enemy_1182_flasrt"]
+	},
+	"flasrt_2" : {
+		"name" : "flasrt_2",
+		"skin" : ["enemy_1182_flasrt_2"]
+	},
+	"flasrt_3" : {
+		"name" : "flasrt_3",
+		"skin" : ["enemy_1182_flasrt_3"]
+	},
+	"mlasrt" : {
+		"name" : "mlasrt",
+		"skin" : ["enemy_1183_mlasrt"]
+	},
+	"mlasrt_2" : {
+		"name" : "mlasrt_2",
+		"skin" : ["enemy_1183_mlasrt_2"]
+	},
+	"mlasrt_3" : {
+		"name" : "mlasrt_3",
+		"skin" : ["enemy_1183_mlasrt_3"]
+	},
+	"cadkgt" : {
+		"name" : "cadkgt",
+		"skin" : ["enemy_1184_cadkgt"]
+	},
+	"cadkgt_2" : {
+		"name" : "cadkgt_2",
+		"skin" : ["enemy_1184_cadkgt_2"]
+	},
+	"cadkgt_3" : {
+		"name" : "cadkgt_3",
+		"skin" : ["enemy_1184_cadkgt_3"]
+	},
+	"nmekgt" : {
+		"name" : "nmekgt",
+		"skin" : ["enemy_1185_nmekgt"]
+	},
+	"nmekgt_2" : {
+		"name" : "nmekgt_2",
+		"skin" : ["enemy_1185_nmekgt_2"]
+	},
+	"nmekgt_3" : {
+		"name" : "nmekgt_3",
+		"skin" : ["enemy_1185_nmekgt_3"]
+	},
+	"bldbld" : {
+		"name" : "bldbld",
+		"skin" : ["enemy_1186_bldbld"]
+	},
 	"skulsr" : {
 		"name" : "skulsr",
 		"skin" : ["enemy_1500_skulsr"]
@@ -738,6 +1490,10 @@ var enemyData = {
 	"crowns" : {
 		"name" : "crowns",
 		"skin" : ["enemy_1502_crowns"]
+	},
+	"talula" : {
+		"name" : "talula",
+		"skin" : ["enemy_1503_talula"]
 	},
 	"cqbw" : {
 		"name" : "cqbw",
@@ -770,5 +1526,125 @@ var enemyData = {
 	"mdrock" : {
 		"name" : "mdrock",
 		"skin" : ["enemy_1511_mdrock"]
+	},
+	"mcmstr" : {
+		"name" : "mcmstr",
+		"skin" : ["enemy_1512_mcmstr"]
+	},
+	"dekght" : {
+		"name" : "dekght",
+		"skin" : ["enemy_1513_dekght"]
+	},
+	"dekght_2" : {
+		"name" : "dekght_2",
+		"skin" : ["enemy_1513_dekght_2"]
+	},
+	"smephi" : {
+		"name" : "smephi",
+		"skin" : ["enemy_1514_smephi"]
+	},
+	"bsnake" : {
+		"name" : "bsnake",
+		"skin" : ["enemy_1515_bsnake"]
+	},
+	"jakill" : {
+		"name" : "jakill",
+		"skin" : ["enemy_1516_jakill"]
+	},
+	"xi" : {
+		"name" : "xi",
+		"skin" : ["enemy_1517_xi"]
+	},
+	"dusk" : {
+		"name" : "dusk",
+		"skin" : ["enemy_1518_dusk"]
+	},
+	"bgball" : {
+		"name" : "bgball",
+		"skin" : ["enemy_1519_bgball"]
+	},
+	"empgrd" : {
+		"name" : "empgrd",
+		"skin" : ["enemy_1520_empgrd"]
+	},
+	"dslily" : {
+		"name" : "dslily",
+		"skin" : ["enemy_1521_dslily"]
+	},
+	"captan" : {
+		"name" : "captan",
+		"skin" : ["enemy_1522_captan"]
+	},
+	"mandra" : {
+		"name" : "mandra",
+		"skin" : ["enemy_1523_mandra"]
+	},
+	"bldkgt" : {
+		"name" : "bldkgt",
+		"skin" : ["enemy_1524_bldkgt"]
+	},
+	"duckmi" : {
+		"name" : "duckmi",
+		"skin" : ["enemy_2001_duckmi"]
+	},
+	"bearmi" : {
+		"name" : "bearmi",
+		"skin" : ["enemy_2002_bearmi"]
+	},
+	"rockman" : {
+		"name" : "rockman",
+		"skin" : ["enemy_2003_rockman"]
+	},
+	"balloon" : {
+		"name" : "balloon",
+		"skin" : ["enemy_2004_balloon"]
+	},
+	"axetro" : {
+		"name" : "axetro",
+		"skin" : ["enemy_2005_axetro"]
+	},
+	"flsnip" : {
+		"name" : "flsnip",
+		"skin" : ["enemy_2006_flsnip"]
+	},
+	"flwitch" : {
+		"name" : "flwitch",
+		"skin" : ["enemy_2007_flwitch"]
+	},
+	"flking" : {
+		"name" : "flking",
+		"skin" : ["enemy_2008_flking"]
+	},
+	"upeopl" : {
+		"name" : "upeopl",
+		"skin" : ["enemy_3001_upeopl"]
+	},
+	"ftrtal" : {
+		"name" : "ftrtal",
+		"skin" : ["enemy_3002_ftrtal"]
+	},
+	"alymot" : {
+		"name" : "alymot",
+		"skin" : ["enemy_3003_alymot"]
+	},
+	"toxcar" : {
+		"name" : "toxcar",
+		"skin" : ["enemy_4001_toxcar"]
+	},
+	"toxcar_2" : {
+		"name" : "toxcar_2",
+		"skin" : ["enemy_4001_toxcar_2"]
+	},
+	"syokai" : {
+		"name" : "syokai",
+		"skin" : ["enemy_4002_syokai"]
+	},
+	"syokai_2" : {
+		"name" : "syokai_2",
+		"skin" : ["enemy_4002_syokai_2"]
+	},
+	"pbank" : {
+		"name" : "pbank",
+		"skin" : ["enemy_4003_pbank"]
 	},
 };
